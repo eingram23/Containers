2bd58de223a8060a4573eea471f136dcac46b9f0
Edward Ingram <edward.ingram23@gmail.com>
2022-06-28 00:25:57 -0700

    removed use of latest tag on imagees

---
+++ Projects/vault-dev-server/build.sh
@@ -0,0 +1,1 @@
+podman run -d --name=vault-dev-server --pod=vault -e VAULT_ADDR='http://0.0.0.0:8200' -e VAULT_DEV_ROOT_TOKEN_ID='dev-token' --cap-add=IPC_LOCK vault:latest
--- Projects/www.ycdisp.com/build_ycdisp.sh
+++ Projects/www.ycdisp.com/build_ycdisp.sh
@@ -1,6 +1,6 @@
 podman pod create --name yc -p 8081:80
 podman run -d --restart=always --name=yc-web --pod=yc -v /home/eingram/code/Containers/test.ycdisp.com/www/html:/var/www/html:z docker.io/kungfujoe23/yc-web:latest
-podman run -d --restart=always --pod=yc --name=yc-db -v yc-db-data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=NL2B1r13 mariadb:latest
+podman run -d --restart=always --pod=yc --name=yc-db -v yc-db-data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=NL2B1r13 mariadb:10.7.4
 
 # docker exec -i some-mysql sh -c 'exec mysql -uroot -p"$MYSQL_ROOT_PASSWORD"' < /some/path/on/your/host/all-databases.sql
 # # setsebool -P httpd_can_network_relay 1
