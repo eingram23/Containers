d86bce0657e9043097832a139ff7af5965ac98b3
Edward Ingram <edward.ingram23@gmail.com>
2022-07-27 21:04:42 -0700

    Moved build scripts to Scripts repo

--- test.diversionforum.net/push_docker.io.sh
+++
@@ -1,3 +0,0 @@
-podman login docker.io
-podman tag df-web docker.io/kungfujoe23/df-web:latest
-podman push docker.io/kungfujoe23/df-web:latest
--- www.ycdisp.com/pod_systemd.sh
+++
@@ -1,6 +0,0 @@
-loginctl enable-linger
-mkdir -p ~/.config/systemd/user
-cd ~/.config/systemd/user
-podman generate systemd --name yc --files --new
-systemctl --user daemon-reload
-systemctl --user enable --now pod-yc.service
