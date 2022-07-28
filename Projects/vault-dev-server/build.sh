5866f164770fb5b4aaa4d916b5202b849d62defd
Edward Ingram <edward.ingram23@gmail.com>
2022-07-27 19:49:55 -0700

    Removed Projects folder

--- Projects/vault-dev-server/build.sh
+++
@@ -1,1 +0,0 @@
-podman run -d --name=vault-dev-server --pod=vault -e VAULT_ADDR='http://0.0.0.0:8200' -e VAULT_DEV_ROOT_TOKEN_ID='dev-token' --cap-add=IPC_LOCK vault:latest
--- Projects/www.ycdisp.com/push_docker.io.sh
+++
@@ -1,3 +0,0 @@
-podman login docker.io
-podman tag yc-web docker.io/kungfujoe23/yc-web:latest
-podman push docker.io/kungfujoe23/yc-web:latest
